f107d17fc8410d1ff2f1fe254b73a18f567cc311
BradJ <j.chaiyaphop@gmail.com>
2021-09-16 13:40:25 +0700

    modified:   main.py

--- main.py
+++ main.py
@@ -3,10 +3,26 @@ import numpy as np
 arr1 = list(range(1000))
 arr2 = list(range(1000,2000))
 
+#pure python
 result1 = 0
 for x1, x2 in zip(arr1,arr2):
     result1 += x1*x2
 print(result1)
 
+#use numpy
 result2 = np.dot(arr1,arr2)
 print(result2)
+
+#array 3D
+arr = np.array([
+    [
+        [1,2,3,4],
+        [1,2,3,4]
+    ],
+
+    [
+        [1,2,3,4],
+        [1,2,3,4]
+    ]
+])
+print(arr.shape)
